85d6b77f93f5cc12d1318dfbe068f072110f9fbd
Marko Konsa <marko.konsa@fob-solutions.com>
2016-12-23 21:28:28 +0200

    [PR0030-332] Added basic actions to android, ios, desktop

--- src/main/groovy/io/cify/framework/actions/ActionsDesktopWeb.groovy
+++ src/main/groovy/io/cify/framework/actions/ActionsDesktopWeb.groovy
@@ -1,44 +1,170 @@
 package io.cify.framework.actions
 
-import io.cify.framework.annotations.Title
-import io.cify.framework.core.CifyFrameworkException
 import io.cify.framework.core.Device
+import io.cify.framework.core.DeviceCategory
+import io.cify.framework.core.DeviceManager
+import org.openqa.selenium.JavascriptExecutor
 import org.openqa.selenium.WebElement
+import org.openqa.selenium.interactions.internal.Coordinates
+import org.openqa.selenium.internal.Locatable
+import org.openqa.selenium.support.ui.Select
 
 /**
  * Created by FOB Solutions
  *
  * This class contains actions implementation for DesktopWeb
  */
-class ActionsDesktopWeb implements IActions {
+trait ActionsDesktopWeb implements IActions {
 
-    private Device device
-
-    ActionsDesktopWeb(Device device) {
-        this.device = device
+    /**
+     * Opens URL
+     */
+    void open(String url) {
+        Device device = DeviceManager.getInstance().getActiveDevice(DeviceCategory.BROWSER)
+        device.getDriver().get(url)
     }
 
-    @Override
-    @Title("Click element")
-    public void click(WebElement element) {
-        element.click()
+    /**
+     * Scrolls element into view
+     * @param element
+     */
+    void scrollIntoView(WebElement element) {
+        Device device = DeviceManager.getInstance().getActiveDevice(DeviceCategory.BROWSER)
+        ((JavascriptExecutor) device.getDriver()).executeScript("arguments[0].scrollIntoView(true);", element)
     }
 
-    @Override
-    @Title("Tap - not supported in DesktopWeb")
-    public void tap(WebElement element) {
-        throw new CifyFrameworkException("Tap is not supported in DesktopWeb")
+    /**
+     * Scrolls on web page based on pixel values
+     *
+     * @param x pixel value
+     * @param y pixel value
+     */
+    void scrollTo(int x, int y) {
+        Device device = DeviceManager.getInstance().getActiveDevice(DeviceCategory.BROWSER)
+        ((JavascriptExecutor) device.getDriver()).executeScript("scrollTo(" + x + "," + y + ")")
     }
 
-    @Override
-    @Title("Navigate back")
-    public void navigateBack() {
+    /**
+     * Scrolls to element, gets coordinates based on element
+     *
+     * @param element element to scroll to
+     */
+    void scrollToElement(WebElement element) {
+        Coordinates c = ((Locatable) element).getCoordinates()
+        c.onPage()
+        c.inViewPort()
+    }
+
+    /**
+     * Scrolls vertically inside element, expects child element to be lower that parent element
+     * Calculates difference between child and parent element and scrolls in the parent element that much
+     *
+     * @param parent element inside what to scroll
+     * @param child element to scroll to
+     */
+    void scrollVerticallyInElement(WebElement parent, WebElement child) {
+        Device device = DeviceManager.getInstance().getActiveDevice(DeviceCategory.BROWSER)
+        int y = child.getLocation().getY() - parent.getLocation().getY()
+        ((JavascriptExecutor) device.getDriver()).executeScript("arguments[0].scrollTop = arguments[1];", parent, y)
+    }
+
+    /**
+     * Gets bounds of a web element
+     *
+     * @param we WebElement to get bounds of
+     * @return string array list [from left, from top, width, height]
+     */
+    List<String> getBoundedRectangleOfElement(WebElement we) {
+        Device device = DeviceManager.getInstance().getActiveDevice(DeviceCategory.BROWSER)
+        JavascriptExecutor je = (JavascriptExecutor) device.getDriver()
+        List<String> bounds = (ArrayList<String>) je.executeScript(
+                "var rect = arguments[0].getBoundingClientRect();" +
+                        "return [ '' + parseInt(rect.left), '' + parseInt(rect.top), '' + parseInt(rect.width), '' + parseInt(rect.height) ]", we)
+        bounds
+    }
+
+    /**
+     * Gets current URL of the web browser
+     */
+    String getCurrentUrl() {
+        Device device = DeviceManager.getInstance().getActiveDevice(DeviceCategory.BROWSER)
+        device.getDriver().getCurrentUrl()
+    }
+
+    /**
+     * Gets title of the website
+     */
+    String getTitle() {
+        Device device = DeviceManager.getInstance().getActiveDevice(DeviceCategory.BROWSER)
+        device.getDriver().getTitle()
+    }
+
+    /**
+     * Navigates back
+     */
+    void back() {
+        Device device = DeviceManager.getInstance().getActiveDevice(DeviceCategory.BROWSER)
         device.getDriver().navigate().back()
     }
 
-    @Override
-    @Title("Fill in element with text")
-    public void fillIn(WebElement element, String text) {
-        element.sendKeys(text)
+    /**
+     * Navigates forward
+     */
+    void forward() {
+        Device device = DeviceManager.getInstance().getActiveDevice(DeviceCategory.BROWSER)
+        device.getDriver().navigate().forward()
+    }
+
+    /**
+     * Refreshes the page
+     */
+    void refresh() {
+        Device device = DeviceManager.getInstance().getActiveDevice(DeviceCategory.BROWSER)
+        device.getDriver().navigate().refresh()
+    }
+
+    /**
+     * Selects from dropdown by visible text
+     * @param text
+     */
+    void selectByVisibleText(WebElement element, String text) {
+        Select select = new Select(element)
+        select.deselectAll()
+        select.selectByVisibleText(text)
+    }
+
+    /**
+     * Selects from dropdown by value
+     * @param value
+     */
+    void selectByValue(WebElement element, String value) {
+        Select select = new Select(element)
+        select.deselectAll()
+        select.selectByValue(value)
+    }
+
+    /**
+     * Deselects all from select box
+     */
+    void deselectAllFromSelect(WebElement element) {
+        Select select = new Select(element)
+        select.deselectAll()
+    }
+
+    /**
+     * Gets all selected elements
+     * @param element
+     * @return list of select box elements
+     */
+    List<WebElement> getSelectedElements(WebElement element) {
+        Select select = new Select(element)
+        select.getAllSelectedOptions()
+    }
+
+    /**
+     * Checks if radio button is selected
+     */
+    boolean isSelected(WebElement element) {
+        element.isSelected()
     }
 }
--- src/main/groovy/io/cify/framework/actions/ActionsMobileIOSApp.groovy
+++ src/main/groovy/io/cify/framework/actions/ActionsMobileIOSApp.groovy
@@ -1,11 +1,18 @@
 package io.cify.framework.actions
 
 import io.appium.java_client.AppiumDriver
+import io.appium.java_client.MobileDriver
+import io.appium.java_client.MultiTouchAction
 import io.appium.java_client.TouchAction
-import io.appium.java_client.ios.IOSElement
-import io.cify.framework.annotations.Title
+import io.appium.java_client.ios.IOSDriver
+import io.cify.framework.core.CifyFrameworkException
 import io.cify.framework.core.Device
+import io.cify.framework.core.DeviceCategory
+import io.cify.framework.core.DeviceManager
+import org.openqa.selenium.Dimension
+import org.openqa.selenium.WebDriver
 import org.openqa.selenium.WebElement
+import org.openqa.selenium.support.ui.ExpectedCondition
 
 /**
  * Created by FOB Solutions
@@ -13,39 +20,176 @@ import org.openqa.selenium.WebElement
  * This class contains actions implementation for MobileIosApp
  */
 
-class ActionsMobileIOSApp implements IActions {
+trait ActionsMobileIOSApp implements IActions {
 
-    private Device device
-
-    ActionsMobileIOSApp(Device device) {
-        this.device = device
+    /**
+     * Swipe right on an element
+     * @param element WebElement to swipe on
+     * @param duration duration of the swipe in ms
+     */
+    void swipeRight(WebElement element, Integer durationInSeconds) {
+        Device device = DeviceManager.getInstance().getActiveDevice(DeviceCategory.IOS)
+        List rightTopCoordinates = [element.getLocation().getX() + element.getSize().getWidth(), element.getLocation().getY()]
+        List leftTopCoordinates = [element.getLocation().getX(), element.getLocation().getY()]
+        (device.getDriver() as IOSDriver).swipe(leftTopCoordinates[0] + 1, leftTopCoordinates[1] + 1, rightTopCoordinates[0] - 1, rightTopCoordinates[1] + 1, durationInSeconds)
     }
 
-    @Override
-    @Title("Click element")
-    public void click(WebElement element) {
-        element.click()
+    /**
+     * Swipe left on an element
+     * @param element WebElement to swipe on
+     * @param duration duration of the swipe in ms
+     */
+    void swipeLeft(WebElement element, Integer durationInSeconds) {
+        Device device = DeviceManager.getInstance().getActiveDevice(DeviceCategory.IOS)
+        List rightTopCoordinates = [element.getLocation().getX() + element.getSize().getWidth(), element.getLocation().getY()]
+        List leftTopCoordinates = [element.getLocation().getX(), element.getLocation().getY()]
+        (device.getDriver() as IOSDriver).swipe(rightTopCoordinates[0] - 1, rightTopCoordinates[1] + 1, leftTopCoordinates[0] + 1, leftTopCoordinates[1] + 1, durationInSeconds)
     }
 
-    @Override
-    @Title("Tap")
-    public void tap(WebElement element) {
-        new TouchAction((AppiumDriver) device.getDriver()).tap(element).perform()
+    /**
+     * Tilts the map
+     * @param device Device
+     */
+    void tilt() {
+        Device device = DeviceManager.getInstance().getActiveDevice(DeviceCategory.IOS)
+        TouchAction action1 = new TouchAction(device.getDriver() as MobileDriver)
+        TouchAction action2 = new TouchAction(device.getDriver() as MobileDriver)
+        MultiTouchAction action = new MultiTouchAction(device.getDriver() as MobileDriver)
+        Dimension screenSize = device.getDriver().manage().window().getSize()
+
+        action1.press((screenSize.getWidth() * 0.4).toInteger(), (screenSize.getHeight() * 0.5).toInteger()).moveTo(0, (-screenSize.getHeight() * 0.2).toInteger()).release()
+        action2.press((screenSize.getWidth() * 0.6).toInteger(), (screenSize.getHeight() * 0.5).toInteger()).moveTo(0, (-screenSize.getHeight() * 0.2).toInteger()).release()
+
+        action.add(action1).add(action2).perform()
     }
 
-    @Override
-    @Title("Navigate back")
-    public void navigateBack() {
-        device.getDriver().navigate().back()
+    /**
+     * Scrolls down
+     * */
+    void scrollDown() {
+        Device device = DeviceManager.getInstance().getActiveDevice(DeviceCategory.IOS)
+        Dimension dimension = device.getDriver().manage().window().getSize()
+        int bottomY = dimension.getHeight() - 400
+        (device.getDriver() as AppiumDriver).swipe(dimension.getWidth(), dimension.getHeight(), dimension.getWidth(), bottomY, 1000);
     }
 
-    @Override
-    @Title("Fill in element with text")
-    public void fillIn(WebElement element, String text) {
-        if (text.contains("@")) {
-            ((IOSElement) element).setValue(text)
-        } else {
-            element.sendKeys(text)
+    /**
+     * Swipes from right to left
+     * @param device
+     */
+    void swipeRightToLeft() {
+        Device device = DeviceManager.getInstance().getActiveDevice(DeviceCategory.IOS)
+        Dimension screenSize = device.getDriver().manage().window().getSize()
+        int startX = (screenSize.getWidth() * 0.2)
+        int startY = (screenSize.getHeight() * 0.5)
+        int endX = (screenSize.getWidth() * 0.8)
+        int endY = (screenSize.getHeight() * 0.5)
+        (device.getDriver() as MobileDriver).swipe(startX, startY, endX, endY, 1000)
+    }
+
+    /**
+     * Taps in the middle of the screen (X*Y/2)
+     * @param device
+     */
+    void tapInTheMiddleOfScreen() {
+        Device device = DeviceManager.getInstance().getActiveDevice(DeviceCategory.IOS)
+        Dimension screenSize = device.getDriver().manage().window().getSize()
+        int X = screenSize.getWidth() / 2
+        int Y = screenSize.getHeight() / 2
+        (device.getDriver() as MobileDriver).tap(1, X, Y, 1)
+    }
+
+    /**
+     * Double taps element
+     *
+     * @param device - device to perform the tap
+     * @param element - element to double tap
+     * */
+    void doubleTapElement(WebElement element) {
+        Device device = DeviceManager.getInstance().getActiveDevice(DeviceCategory.IOS)
+        MobileDriver driver = device.getDriver() as MobileDriver
+        MultiTouchAction multiTouch = new MultiTouchAction(driver)
+        TouchAction action = new TouchAction(driver).press(element).release().press(element).release()
+        try {
+            multiTouch.add(action).perform()
+        } catch (all) {
+            throw new CifyFrameworkException("Failed to double tap element cause $all.message")
         }
     }
+
+    /**
+     * Zooms in on given element
+     * @param device
+     * @param element
+     */
+    void zoom(WebElement element) {
+        Device device = DeviceManager.getInstance().getActiveDevice(DeviceCategory.IOS)
+        (device.getDriver() as AppiumDriver).zoom(element)
+    }
+
+    /**
+     * Zooms out on given element
+     * @param device
+     * @param element
+     */
+    void pinch(WebElement element) {
+        Device device = DeviceManager.getInstance().getActiveDevice(DeviceCategory.IOS)
+        (device.getDriver() as AppiumDriver).pinch(element)
+    }
+
+    /**
+     * Automatically dismiss alerts
+     * */
+    void autoAcceptAlerts() {
+        Device device = DeviceManager.getInstance().getActiveDevice(DeviceCategory.IOS)
+
+        boolean accept = true
+
+        while (accept) {
+            try {
+                waitForCondition(device, new ExpectedCondition<Boolean>() {
+                    public Boolean apply(WebDriver d) {
+                        try {
+                            (device.getDriver() as MobileDriver).switchTo().alert().accept()
+                            return true
+                        } catch (ignored) {
+                            return false
+                        }
+                    }
+                }, 10)
+            } catch (ignored) {
+                accept = false
+            }
+        }
+    }
+
+    /**
+     * Long tap on element
+     *
+     * @param device - device to perform the long tap
+     * @param element - element to long tap
+     */
+    static void longTap(WebElement element) {
+        Device device = DeviceManager.getInstance().getActiveDevice(DeviceCategory.IOS)
+        MobileDriver driver = device.getDriver() as MobileDriver
+        TouchAction action = new TouchAction(driver).longPress(element)
+        try {
+            action.perform()
+        } catch (all) {
+            throw new CifyFrameworkException("Failed to long tap element cause $all.message")
+        }
+    }
+
+    /**
+     * Hides soft keyboard
+     * @param device
+     */
+    static void hideKeyboard() {
+        Device device = DeviceManager.getInstance().getActiveDevice(DeviceCategory.IOS)
+        try {
+            (device.getDriver() as MobileDriver).hideKeyboard()
+        } catch (ignored) {
+        }
+    }
+
 }
