7fcf1c2874d2c8e9bda17d1f663970fb3fcbb310
vadim-fob <vadim.sinchukov@fob-solutions.com>
2017-01-12 14:00:13 +0200

    [PR0030-318] added devices unique list to report

--- src/main/groovy/io/cify/framework/reporting/Report.groovy
+++ src/main/groovy/io/cify/framework/reporting/Report.groovy
@@ -6,8 +6,6 @@ import org.apache.logging.log4j.Marker
 import org.apache.logging.log4j.MarkerManager
 import org.apache.logging.log4j.core.Logger
 
-import java.text.DecimalFormat
-
 /**
  * Created by FOB Solutions
  *
@@ -16,9 +14,6 @@ import java.text.DecimalFormat
 class Report extends TestReportManager {
     private static final Logger LOG = LogManager.getLogger(this.class) as Logger
     private static final Marker MARKER = MarkerManager.getMarker('REPORT') as Marker
-
-  //  private final static long MILLI = 1000
-  //  private final static DecimalFormat DECIMAL_FORMAT = new DecimalFormat("0.000")
 
     /**
      * Builds json object with test step report information
@@ -71,23 +66,23 @@ class Report extends TestReportManager {
                 scenarioName: scenario.name,
                 cucumberScenarioId: scenario.cucumberScenarioId,
                 startDate: scenario.startDate,
-                passedSteps: scenario.stepList.findAll{ it.result == "passed" }.size(),
-                failedSteps: scenario.stepList.findAll{ it.result == "failed" }.size(),
-                skippedSteps: scenario.stepList.findAll{ it.result == "skipped" }.size(),
+                passedSteps: scenario.stepList.findAll { it.result == "passed" }.size(),
+                failedSteps: scenario.stepList.findAll { it.result == "failed" }.size(),
+                skippedSteps: scenario.stepList.findAll { it.result == "skipped" }.size(),
                 devices: deviceList.collect { it },
                 steps: scenario.stepList.collect {
                     [
-                            stepId    : it.stepId,
-                            stepName  : it.name,
-                            stepResult: it.result,
-                            actions: it.stepActionsList.collect {
+                            stepId          : it.stepId,
+                            stepName        : it.name,
+                            stepResult      : it.result,
+                            actions         : it.stepActionsList.collect {
                                 [
-                                        actionId: it.actionId,
+                                        actionId      : it.actionId,
                                         actionName    : it.name,
                                         actionDuration: it.duration
                                 ]
                             },
-                            stepDuration: it.duration,
+                            stepDuration    : it.duration,
                             stepErrorMessage: formatErrorMessage(it.errorMessage)
                     ]
                 },
@@ -113,29 +108,41 @@ class Report extends TestReportManager {
                 cucumberTestrunId: testRun.cucumberTestrunId,
                 startDate: testRun.startDate,
                 capabilitiesId: testRun.capabilitiesId,
-                passedScenarios: testRun.scenarioList.findAll{ it.result == "passed" }.size(),
-                failedScenarios: testRun.scenarioList.findAll{ it.result == "failed" }.size(),
-                passedSteps: sumOfStepsInTestrun(testRun,"passed"),
-                failedSteps: sumOfStepsInTestrun(testRun,"failed"),
-                skippedSteps: sumOfStepsInTestrun(testRun,"skipped"),
+                passedScenarios: testRun.scenarioList.findAll { it.result == "passed" }.size(),
+                failedScenarios: testRun.scenarioList.findAll { it.result == "failed" }.size(),
+                passedSteps: sumOfAllStepsInTestrun(testRun, "passed"),
+                failedSteps: sumOfAllStepsInTestrun(testRun, "failed"),
+                skippedSteps: sumOfAllStepsInTestrun(testRun, "skipped"),
                 scenarios: testRun.scenarioList.collect {
                     [
-                            scenarioId    : it.scenarioId,
-                            scenarioName  : it.name,
-                            scenarioResult: it.result,
-                            cucumberScenarioId    : it.cucumberScenarioId,
-                            devices: it.deviceList.collect { it }
+                            scenarioId        : it.scenarioId,
+                            scenarioName      : it.name,
+                            scenarioResult    : it.result,
+                            cucumberScenarioId: it.cucumberScenarioId,
+                            devices           : it.deviceList.collect { it }
                     ]
                 },
+                testrunDevices: getUniqueDeviceList(testRun).size(),
+                testrunDeviceList: getUniqueDeviceList(testRun),
                 endDate: testRun.endDate,
                 duration: testRun.duration, result: testRun.result)
         LOG.info(MARKER, jsonBuilder.toPrettyString())
         return jsonBuilder.toString()
     }
 
-    private static int sumOfStepsInTestrun(TestRun testRun, String status){
+    private static List getUniqueDeviceList(TestRun testRun) {
+        List list = new ArrayList<>()
+        testRun?.scenarioList?.each {
+            it.deviceList?.each {
+                list.add(it)
+            }
+        }
+        return list.unique()
+    }
+
+    private static int sumOfAllStepsInTestrun(TestRun testRun, String status) {
         int result = 0
-        testRun?.scenarioList?.each{ result = result + it.stepList.findAll{ it.result == status }.size() }
+        testRun?.scenarioList?.each { result = result + it.stepList.findAll { it.result == status }.size() }
         return result
     }
 
@@ -143,7 +150,4 @@ class Report extends TestReportManager {
         return errorMessage ? errorMessage : ""
     }
 
-/*    private static String formatDuration(long duration) {
-        return duration ? DECIMAL_FORMAT.format(duration / MILLI) : 0
-    }*/
 }
