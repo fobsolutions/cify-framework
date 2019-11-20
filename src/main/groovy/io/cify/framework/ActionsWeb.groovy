592cfdada348332df4e83e83552740aa7139a2fc
Alexander Epifanov <alexander.epifanov@here.com>
2019-11-20 13:06:02 +0100

    Added new proxy classes for actions to use implementations inside them outside of the framework

---
+++ src/main/groovy/io/cify/framework/ActionsWeb.groovy
@@ -0,0 +1,6 @@
+package io.cify.framework
+
+import io.cify.framework.actions.ActionsDesktopWeb
+import io.cify.framework.actions.IActions
+
+public class ActionsWeb extends ActionsBase implements ActionsDesktopWeb {}
